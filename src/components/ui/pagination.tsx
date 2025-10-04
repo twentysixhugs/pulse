0731532f4f1d1afcc531c402906a9771b42e58ac
EduLover <Vanya.Kotick17@gmail.com>
2025-10-04 20:12:01 +0000

    Сделай просто чтобы шрифт был больше в "назад" и "далее", а в цифрах был

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -67,7 +67,7 @@ const PaginationPrevious = ({
   <PaginationLink
     aria-label="Go to previous page"
     size="default"
-    className={cn("gap-1 pl-2.5", className)}
+    className={cn("gap-1 pl-2.5 text-base", className)}
     {...props}
   >
     <ChevronLeft className="h-4 w-4" />
@@ -83,7 +83,7 @@ const PaginationNext = ({
   <PaginationLink
     aria-label="Go to next page"
     size="default"
-    className={cn("gap-1 pr-2.5", className)}
+    className={cn("gap-1 pr-2.5 text-base", className)}
     {...props}
   >
     <span>Далее</span>
