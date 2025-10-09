afb81e93eb005a25af1c670f105e81957eea18fd
EduLover <Vanya.Kotick17@gmail.com>
2025-10-09 17:39:06 +0000

    Короче вместо использования flex-wrap и прочего, сделай так чтобы третья

--- src/app/trader/page.tsx
+++ src/app/trader/page.tsx
@@ -79,16 +79,16 @@ export default function TraderPage() {
 
     return (
         <Tabs defaultValue="profile" className="w-full">
-            <TabsList className="flex flex-wrap h-auto">
-                <TabsTrigger value="profile" className="flex-1">
+            <TabsList className="grid h-auto grid-cols-2 sm:grid-cols-3">
+                <TabsTrigger value="profile">
                     <UserIcon className="mr-2 h-4 w-4" />
                     Профиль
                 </TabsTrigger>
-                <TabsTrigger value="rating" className="flex-1">
+                <TabsTrigger value="rating">
                     <BarChart className="mr-2 h-4 w-4" />
                     Рейтинг
                 </TabsTrigger>
-                <TabsTrigger value="all-alerts" className="flex-1">
+                <TabsTrigger value="all-alerts">
                     <Flame className="mr-2 h-4 w-4" />
                     Все алерты
                 </TabsTrigger>
