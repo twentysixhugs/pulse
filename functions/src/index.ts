211befe5b9544267cddec83f1dfcdf4e21f7727e
EduLover <Vanya.Kotick17@gmail.com>
2025-10-15 17:03:46 +0000

    I added the env

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,25 +10,22 @@ getFirestore();
 
 // Получаем токен из переменных окружения
 const botToken = process.env.TELEGRAM_BOT_TOKEN;
+
 if (!botToken) {
   console.error("Telegram bot token is not set in environment variables.");
+  throw new Error("Telegram bot token is not set");
 }
 
-const bot = new Telegraf(botToken!);
+const bot = new Telegraf(botToken);
 
 bot.start((ctx) => ctx.reply("Добро пожаловать!"));
 
 // HTTP-функция для вебхука
 export const telegramWebhook = functions.https.onRequest(async (req, res) => {
-  if (bot) {
     try {
-      await bot.handleUpdate(req.body);
-      res.sendStatus(200);
+      await bot.handleUpdate(req.body, res);
     } catch (e) {
       console.error(e);
       res.sendStatus(500);
     }
-  } else {
-    res.sendStatus(500);
-  }
 });
